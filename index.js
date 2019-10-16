928aa55fe6f5a38c8d69b0af1a6879c17b827df9
Sasa <weiting@eiTingde-MBP.Home>
2019-10-16 13:16:51 +0800

    modify nodejs server

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
+const http = require("http");
 
-var handle = {}
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
+const hostname = "127.0.0.1";
+const PORT = process.env.PORT || 5000;
 
-server.start(router.route, handle);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\n");
+});
+
+server.listen(PORT, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
